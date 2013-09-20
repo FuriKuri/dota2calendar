25f52743f82c825b6ce8a9821fa6aa7b0072388d
Theo Pack <tf.pack@googlemail.com>
2013-09-20 22:07:12 +0200

    delete libs folder

--- src/main/java/com/github/furikuri/dota2calendar/HelloActivity.java
+++ src/main/java/com/github/furikuri/dota2calendar/HelloActivity.java
@@ -2,6 +2,7 @@ package com.github.furikuri.dota2calendar;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import com.google.gson.Gson;
 
@@ -17,7 +18,8 @@ public class HelloActivity extends Activity {
 	public void onStart() {
         super.onStart();
         TextView textView = (TextView) findViewById(R.id.text_view);
-        textView.setText("Hello world!!!!");
+        textView.setText("Hello world!!!! Dota" + BuildConfig.DEBUG);
+        Log.d("bla", "debug" + BuildConfig.DEBUG);
         Gson gson = new Gson();
     }
 
